3bb6537e5dd52474e68af19328578ceeb52879a1
Taormina7575 <vinnie.taormina@gmail.com>
2021-04-21 16:00:10 -0400

    last

--- index.js
+++ index.js
@@ -39,15 +39,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  Person.prototype.eat = function(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
   }
  
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
  
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
+  }
+  
+  const newPerson = new Person('vinnie', 18)
 
-  
-  
-  
   
   /*
     TASK 2
@@ -63,10 +75,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+  }
+
+
   
   /*
     TASK 3
@@ -75,18 +95,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  }
+  
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
   }
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Window binding is when the this keyword refers to the global enviroment and is generally considered an error.
+    2. Implicit Binding is when this refers to the object left of the dot.
+    3. New binding is when this refers to the object created by the constructor function.
+    4. Explicit binding is when the this keyword refers to the object defined by call, apply, or bind.
   */
   
   
